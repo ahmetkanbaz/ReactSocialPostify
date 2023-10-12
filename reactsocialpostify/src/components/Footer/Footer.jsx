77bb30f89642e5914dce9684eba9a613425b8bee
ahmetkanbaz <ahmetkanbaz@gmail.com>
2023-10-12 23:42:13 +0300

    Update Footer.jsx

--- reactsocialpostify/src/components/Footer/Footer.jsx
+++ reactsocialpostify/src/components/Footer/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <div className="container">
         <div className="row align-items-center">
           <div className="col-md-4 col-sm-6 col-12">
-            <div className="bg-danger"> Footer</div>
+            <div>Footer</div>
           </div>
           <div className="col-md-4 col-sm-6 col-12">
-            <div className="bg-info"> Footer</div>
+            <div> Footer</div>
           </div>
           <div className="col-md-4 col-sm-6 col-12">
             <h5>İletişime Geçin</h5>
